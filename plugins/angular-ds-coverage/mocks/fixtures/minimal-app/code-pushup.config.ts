e96ac00a8df640cab3a1a89c905affe851d01a72
Michael <michael.hladky@push-based.io>
2025-02-03 12:31:14 +0100

    wip

--- plugins/angular-ds-coverage/mocks/fixtures/minimal-app/code-pushup.config.ts
+++ plugins/angular-ds-coverage/mocks/fixtures/minimal-app/code-pushup.config.ts
@@ -1,19 +1,25 @@
 import { angularDsCoveragePluginCoreConfig } from '../../../src/core.config';
 
-export default angularDsCoveragePluginCoreConfig({
-  directory: 'libs/design-system',
-  dsComponents: [
-    {
-      docsUrl:
-        'https://storybook.entaingroup.corp/latest/?path=/docs/components-dropdown--overview',
-      componentName: 'DSDropdown',
-      matchingCssClasses: ['dropdown', 'legacy-dropdown', 'custom-dropdown'],
-    },
-    {
-      docsUrl:
-        'https://storybook.entaingroup.corp/latest/?path=/docs/components-button--overview',
-      componentName: 'DSButton',
-      matchingCssClasses: ['btn', 'btn-dark', 'btn-light'],
-    },
-  ],
-});
+export default {
+  persist: {
+    outputDir: '.code-pushup/angular-ds-coverage-style-formats-demo',
+    format: ['json', 'md'],
+  },
+  ...(await angularDsCoveragePluginCoreConfig({
+    directory: 'libs/design-system',
+    dsComponents: [
+      {
+        docsUrl:
+          'https://storybook.entaingroup.corp/latest/?path=/docs/components-dropdown--overview',
+        componentName: 'DSDropdown',
+        matchingCssClasses: ['dropdown', 'legacy-dropdown', 'custom-dropdown'],
+      },
+      {
+        docsUrl:
+          'https://storybook.entaingroup.corp/latest/?path=/docs/components-button--overview',
+        componentName: 'DSButton',
+        matchingCssClasses: ['btn', 'btn-dark', 'btn-light'],
+      },
+    ],
+  })),
+};
---
+++ plugins/angular-ds-coverage/mocks/fixtures/style-formats/code-pushup.config.ts
@@ -0,0 +1,22 @@
+import { ComponentReplacement } from '../../../src/index';
+import { angularDsCoveragePluginCoreConfig } from '../../../src/core.config';
+
+const dsComponents: ComponentReplacement[] = [
+  {
+    componentName: 'DSButton',
+    matchingCssClasses: ['btn', 'btn-primary', 'legacy-button'],
+    docsUrl:
+      'https://storybook.entaingroup.corp/latest/?path=/docs/components-button--overview',
+  },
+];
+
+export default {
+  persist: {
+    outputDir: '.code-pushup/angular-ds-coverage-style-formats-demo',
+    format: ['json', 'md'],
+  },
+  ...(await angularDsCoveragePluginCoreConfig({
+    directory: 'plugins/angular-ds-coverage/mocks/fixtures/style-formats',
+    dsComponents,
+  })),
+};
