1ab601f5e746cab479c6584331b47a6679c414ee
Michael <michael.hladky@push-based.io>
2025-02-09 16:00:21 +0100

    move fixtures

--- plugins/angular-ds-coverage/mocks/fixtures/css-styles-usage/accordion/README.md
+++
@@ -1,3 +0,0 @@
-# @frontend/ui/accordion
-
-Secondary entry point of `@frontend/ui`. It can be used by importing from `@frontend/ui/accordion`.
--- plugins/angular-ds-coverage/mocks/fixtures/css-styles-usage/accordion/src/testing/src/public_api.ts
+++
@@ -1,3 +0,0 @@
-export * from './accordion.harness';
-export * from './accordion-header.harness';
-export * from './accordion-content.harness';
