1c2c40796936f753a61adcb8b63463f8dab7963a
Michael <michael.hladky@push-based.io>
2025-02-23 16:51:24 +0100

    wip

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-
-
-# Run Audits for Design System
-`nx run sports:code-pushup-ds-report`
+Todos:
+- autogenerate cd components config
+  - manual handling of classes
+- autoconfig config files `code-push.config.ts`
+- fix `nx-plugin`
+- usage stats - count of how often used
+- merge helper
+- bench test asset resolving
+- 
--- plugins/mocks/fixtures/minimal-repo/packages/application/code-pushup.config.ts
+++ plugins/mocks/fixtures/minimal-repo/packages/application/code-pushup.config.ts
@@ -1,5 +1,5 @@
 import { mergeConfigs } from '@code-pushup/utils';
-import { dsComponentUsagePluginCoreConfig } from '../../../../../src';
+import { entainDsComponentUsageConfig } from '../../../../../src/config/entain-ds-components-usage.config';
 
 export default mergeConfigs(
   {
@@ -9,7 +9,7 @@ export default mergeConfigs(
     },
     plugins: [],
   },
-  await dsComponentUsagePluginCoreConfig({
+  await entainDsComponentUsageConfig({
     directory: 'plugins/mocks/fixtures/minimal-repo/packages/application',
     dsComponents: [
       {
@@ -17,79 +17,6 @@ export default mergeConfigs(
         deprecatedCssClasses: ['btn', 'btn-primary', 'legacy-button'],
         docsUrl:
           'https://storybook.entaingroup.corp/latest/?path=/docs/components-button--overview',
-      },
-      {
-        componentName: 'DSTabsModule',
-        deprecatedCssClasses: ['ms-tab-bar', 'legacy-tabs', 'custom-tabs'],
-        docsUrl:
-          'https://storybook.entaingroup.corp/latest/?path=/docs/components-tabsgroup--overview',
-      },
-      {
-        componentName: 'DSCard',
-        deprecatedCssClasses: ['card', 'legacy-card', 'custom-card'],
-        docsUrl:
-          'https://storybook.entaingroup.corp/latest/?path=/docs/components-card--overview',
-      },
-      {
-        componentName: 'DSModal',
-        deprecatedCssClasses: ['modal', 'popup', 'legacy-dialog'],
-        docsUrl:
-          'https://storybook.entaingroup.corp/latest/?path=/docs/components-modal--overview',
-      },
-      {
-        componentName: 'DSInput',
-        deprecatedCssClasses: ['input', 'form-control', 'legacy-input'],
-        docsUrl:
-          'https://storybook.entaingroup.corp/latest/?path=/docs/components-input--overview',
-      },
-      {
-        componentName: 'DSDropdown',
-        deprecatedCssClasses: [
-          'dropdown',
-          'legacy-dropdown',
-          'custom-dropdown',
-        ],
-        docsUrl:
-          'https://storybook.entaingroup.corp/latest/?path=/docs/components-dropdown--overview',
-      },
-      {
-        componentName: 'DSAlert',
-        deprecatedCssClasses: ['alert', 'alert-warning'],
-        docsUrl:
-          'https://storybook.entaingroup.corp/latest/?path=/docs/components-alert--overview',
-      },
-      {
-        componentName: 'DSAlertTooltip',
-        deprecatedCssClasses: ['alert', 'alert-tooltip'],
-        docsUrl:
-          'https://storybook.entaingroup.corp/latest/?path=/docs/components-alert-tooltip--overview',
-      },
-      {
-        componentName: 'DSDropDownButton',
-        deprecatedCssClasses: ['btn', 'btn-dropdown'],
-        docsUrl:
-          'https://storybook.entaingroup.corp/latest/?path=/docs/components-btn-dropdown--overview',
-      },
-      {
-        componentName: 'DSProgressBar',
-        deprecatedCssClasses: [
-          'progress-bar',
-          'loading-bar',
-          'legacy-progress',
-        ],
-        docsUrl:
-          'https://storybook.entaingroup.corp/latest/?path=/docs/components-progressbar--overview',
-      },
-      {
-        componentName: 'DSSlider',
-        deprecatedCssClasses: ['slider', 'range-slider', 'legacy-slider'],
-        docsUrl:
-          'https://storybook.entaingroup.corp/latest/?path=/docs/components-slider--overview',
-      },
-      {
-        componentName: 'DSNavbar',
-        deprecatedCssClasses: ['navbar', 'navigation', 'legacy-navbar'],
-        docsUrl: 'https://storybook.entaingroup.corp/latest/?p',
       },
     ],
   })
