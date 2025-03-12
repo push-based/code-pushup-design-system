3b023352a4338c15e0803c8819420814a13e506e
ondrej.svoren <svoren258@gmail.com>
2025-03-12 13:25:54 +0100

    refactor(): eslint ssr config simplified

--- plugins/src/ssr-adoption/src/eslint.config.ssr.cjs
+++ plugins/src/ssr-adoption/src/eslint.config.ssr.cjs
@@ -1,3 +1,21 @@
-const baseConfig = require('../../../../eslint.config.cjs');
-const ssr = require('../../../../tools/eslint-rules/configs/eslint-ssr.config.cjs');
-module.exports = [...baseConfig, ...ssr];
+const nxEslintPlugin = require('@nx/eslint-plugin');
+
+module.exports = [
+  {
+    plugins: { '@nx': nxEslintPlugin }
+  },
+  {
+    files: ['**/*.ts'],
+    rules: {
+      'no-restricted-globals': ['error', 'document', 'window'],
+      'no-restricted-properties': [
+        'error',
+        {
+          object: 'window',
+          property: 'document',
+        },
+      ],
+      '@nx/workspace-no-this-window-document': 'error',
+    },
+  },
+];
