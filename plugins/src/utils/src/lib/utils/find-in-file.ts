753c9b0902ec92cf3864cbbca6ee68c58973ed49
Michael <michael.hladky@push-based.io>
2025-03-18 12:04:47 +0100

    fix: adjust find in file logic

--- plugins/src/utils/src/lib/utils/find-in-file.ts
+++ plugins/src/utils/src/lib/utils/find-in-file.ts
@@ -1,7 +1,6 @@
 import * as readline from 'node:readline';
 import * as fs from 'node:fs';
 import * as path from 'node:path';
-import * as process from 'node:process';
 
 /**
  * Reads a file line-by-line and checks if it contains the search pattern.
@@ -46,13 +45,7 @@ export async function findFilesWithPattern(
   baseDir: string,
   searchPattern: string
 ) {
-  const l = [];
-  for await (const filePath of findFiles(baseDir)) {
-    if (await fileContainsPattern(filePath, searchPattern)) {
-      l.push(path.join(process.cwd(), filePath));
-    }
-  }
-  return l;
+  return (await findInFiles(baseDir, searchPattern)).map(({ file }) => file);
 }
 
 /**
@@ -87,4 +80,102 @@ export async function* findFiles(
       }
     }
   }
+}
+
+export function* accessContent(content: string): Generator<string> {
+  for (const line of content.split('\n')) {
+    yield line;
+  }
+}
+
+export function getLineHits(
+  content: string,
+  pattern: string,
+  bail = false
+): LinePosition[] {
+  const hits = [];
+  let index = content.indexOf(pattern);
+
+  while (index !== -1) {
+    hits.push({ startColumn: index, endColumn: index + pattern.length });
+    if (bail) {
+      return hits;
+    }
+    index = content.indexOf(pattern, index + 1);
+  }
+  return hits;
+}
+
+export type LinePosition = {
+  startColumn: number;
+  endColumn?: number;
+};
+
+export type SourcePosition = {
+  startLine: number;
+  endLine?: number;
+} & LinePosition;
+
+export type SourceLocation = {
+  file: string;
+  position: SourcePosition;
+};
+
+export async function findInFile(
+  file: string,
+  searchPattern: string,
+  bail = false
+): Promise<SourceLocation[]> {
+  const hits: SourceLocation[] = [];
+  const content = fs.readFileSync(file, 'utf8');
+  let startLine = 0;
+  for (const line of accessContent(content)) {
+    startLine++;
+    getLineHits(line, searchPattern, bail).forEach((position) => {
+      hits.push({
+        file,
+        position: {
+          startLine,
+          ...position,
+        },
+      });
+    });
+  }
+  return hits;
+}
+
+export async function findInFiles(
+  baseDir: string,
+  pattern: string,
+  glob = '.ts$',
+  bail = false
+): Promise<SourceLocation[]> {
+  const globRegex = glob && typeof glob === 'string' ? new RegExp(glob) : glob;
+  const queue: string[] = [baseDir];
+  const results: SourceLocation[] = [];
+
+  while (queue.length > 0) {
+    const dir = queue.shift()!;
+
+    let entries;
+    try {
+      entries = fs.readdirSync(dir, { withFileTypes: true });
+    } catch (error) {
+      console.error(`Error reading directory ${dir}:`, error);
+      continue;
+    }
+
+    for (const entry of entries) {
+      const fullPath = path.join(dir, entry.name);
+      if (entry.isDirectory()) {
+        queue.push(fullPath);
+      } else if (entry.isFile() && fullPath.match(globRegex)) {
+        for (const result of await findInFile(fullPath, pattern, bail)) {
+          results.push(result);
+        }
+      }
+    }
+  }
+
+  return results;
 }
